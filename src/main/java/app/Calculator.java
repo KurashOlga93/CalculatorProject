ce20dfdf6d49e99ed355e0ba4353d4fa01cfa7fd
olgakurash <olga9793@gmail.com>
2025-02-17 17:17:45 +0300

    lesson 21 changes

--- src/main/java/app/Calculator.java
+++ src/main/java/app/Calculator.java
@@ -1,22 +1,24 @@
 package app;
 
-import org.openqa.selenium.By;
-
 public class Calculator {
 
     public double sum(double x, double y) {
+        System.out.println("Sum of " + x + " and " + y);
         return x + y;
     }
 
     public double subtract(double x, double y) {
+        System.out.println("Subtract of " + x + " and " + y);
         return x - y;
     }
 
     public double multiply(double x, double y) {
+        System.out.println("Multiply of " + x + " and " + y);
         return x * y;
     }
 
     public double division(double x, double y) {
+        System.out.println("Division of " + x + " and " + y);
         if (y == 0) {
             System.out.println("Division by zero is prohibited.");
             return 0;
